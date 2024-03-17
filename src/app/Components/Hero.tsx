938c057cd8dfb97d9a62c3dce5fce40867d45fdf
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-03-17 18:50:48 -0500

    Align

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -10,12 +10,12 @@ const Hero = ({ goToCamera }: Props) => {
   return (
     <section>
         <div className='grid wrapper px-4 pb-8 pt-20 lg:gap-8 xl:gap-0 lg:pb-8 lg:pt-30 lg:grid-cols-12'>
-            <div className='mr-auto place-self-center lg:col-span-7'>
-                <h1 className='hidden md:flex items-center gap-3 max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-white'>
+            <div className='flex flex-col justify-center items-center lg:mr-auto lg:place-self-center lg:justify-start lg:items-start lg:col-span-7'>
+                <h1 className='hidden lg:flex items-center gap-3 max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none lg:text-5xl xl:text-6xl text-white'>
                     <Brand />
                 </h1>
-                <p className='max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-gray-400'>Soy una inteligencia artificial especializada en detectar las enfermedades de tus plantas.</p>
-                <button onClick={goToCamera} className='inline-flex gap-3 border items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 focus:ring-primary-900'>
+                <p className='max-w-2xl text-center lg:text-left mb-6 font-light lg:mb-8 lg:text-lg lg:text-xl text-gray-400'>Soy una inteligencia artificial especializada en detectar las enfermedades de tus plantas.</p>
+                <button onClick={goToCamera} className='flex w-52 gap-3 border items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 focus:ring-primary-900'>
                     Examinar planta
                     <Camera />
                 </button>
--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar() {
       <div className='fixed inset-0 bg-gradient-to-b from-white/70 to-slate-950/70 translate-x-full peer-checked:translate-x-0 transition-transform md:static md:translate-x-0 md:bg-none'>
         <ul className='absolute inset-x-0 top-24 p-12 bg-white w-[90%] mx-auto rounded-md h-max text-center grid gap-6 font-bold shadow-2xl md:w-max md:bg-transparent md:p-0 md:grid-flow-col md:static'>
           <li>
-            <Link href='/about' className='text-slate-800 md:text-white px-3 py-2 rounded-md md:hover:bg-slate-500'>Nosotros</Link>
+            <Link href='/' className='text-slate-800 md:text-white px-3 py-2 rounded-md md:hover:bg-slate-500'>Nosotros</Link>
           </li>
           <li>
             <Link href='/' className='text-slate-800 md:text-white px-3 py-2 rounded-md md:hover:bg-slate-500'>Entrevistas</Link>
