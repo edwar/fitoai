2478220d8a574e1aebf6e9dd9fb5b6c6ad02a9ee
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-03-17 11:49:25 -0500

    Footer format code

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -5,15 +5,15 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     <footer className="p-4 md:p-8 lg:p-10 bg-gray-800">
-  <div className="mx-auto max-w-screen-xl text-center">
-      <Link href="/" className="flex gap-3 justify-center items-center text-2xl font-semibold text-white"> 
-          <Logo size='small'/>
-          FitoAI
-      </Link>
-      <p className="my-6 text-gray-400">Aplicacion web potenciada con Inteligencia Artificial que ayuda con la identificación de plagas en los cultivos.</p>
-      <span className="text-sm sm:text-center text-gray-400">2024 - <Link href="/" className="hover:underline">Fito AI</Link>.</span>
-  </div>
-</footer>
+      <div className="mx-auto max-w-screen-xl text-center">
+          <Link href="/" className="flex gap-3 justify-center items-center text-2xl font-semibold text-white"> 
+              <Logo size='small'/>
+              FitoAI
+          </Link>
+          <p className="my-6 text-gray-400">Aplicacion web potenciada con Inteligencia Artificial que ayuda con la identificación de plagas en los cultivos.</p>
+          <span className="text-sm sm:text-center text-gray-400">2024 - <Link href="/" className="hover:underline">Fito AI</Link>.</span>
+      </div>
+    </footer>
   )
 }
 
