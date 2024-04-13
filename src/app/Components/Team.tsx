333804b9a0c92968e90491c02c21f135514e0505
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-04-13 17:17:00 -0500

    Responsive card team

--- src/app/Components/Team.tsx
+++ src/app/Components/Team.tsx
@@ -8,12 +8,12 @@ const Team = () => {
                 <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-white'>Equipo</h2>
                 <p className='font-light lg:mb-16 sm:text-xl text-gray-400'>Somos un equipo de estudiantes de la institucion educativa departamental Hernan Venegas Carrillo</p>
             </div> 
-            <div className='grid px-10 lg:px-0 gap-8 mb-6 lg:mb-16 md:grid-cols-2'>
-                <div className='items-center rounded-lg shadow sm:flex bg-gray-800 border-gray-700 hover:scale-110 duration-700'>
-                   <div>
-                        <img className='w-full rounded-lg sm:rounded-none sm:rounded-l-lg' src='/team/angela.webp' alt='Angela' />
+            <div className='grid px-10 gap-16 mb-6 lg:mb-16 md:grid-cols-2'>
+                <div className='flex flex-col sm:flex-row items-center rounded-lg shadow bg-gray-800 border-gray-700 hover:scale-110 duration-700'>
+                   <div className='w-full sm:w-1/3 h-full'>
+                        <img className='max-h-max h-full aspect-auto rounded-t-lg sm:rounded-none sm:rounded-l-lg' src='/team/angela.webp' alt='Angela' />
                     </div>
-                    <div className='p-5'>
+                    <div className='w-full sm:w-2/3 p-5'>
                         <h3 className='text-xl font-bold tracking-tight text-white line-clamp-1'>
                            <div>Angela Rocio Amaya Diaz</div>
                         </h3>
@@ -21,11 +21,11 @@ const Team = () => {
                         <p className='mt-3 mb-4 font-light text-gray-400 line-clamp-3'>Diseño de pagina y editora de contenidos</p>
                     </div>
                 </div> 
-                <div className='items-center rounded-lg shadow sm:flex bg-gray-800 border-gray-700 hover:scale-110 duration-700'>
-                   <div>
-                        <img className='w-full rounded-lg sm:rounded-none sm:rounded-l-lg' src='/team/santiago.webp' alt='Santiago' />
+                <div className='flex flex-col sm:flex-row items-center rounded-lg shadow bg-gray-800 border-gray-700 hover:scale-110 duration-700'>
+                    <div className='w-full sm:w-1/3 h-full'>
+                        <img className='max-h-max h-full aspect-auto rounded-t-lg sm:rounded-none sm:rounded-l-lg' src='/team/santiago.webp' alt='Santiago' />
                     </div>
-                    <div className='p-5'>
+                    <div className='w-full sm:w-2/3 p-5'>
                         <h3 className='text-xl font-bold tracking-tight text-white line-clamp-1'>
                            <div>Santiago Morales Morales</div>
                         </h3>
@@ -33,11 +33,11 @@ const Team = () => {
                         <p className='mt-3 mb-4 font-light text-gray-400 line-clamp-3'>programador</p>
                     </div>
                 </div> 
-                <div className='items-center rounded-lg shadow sm:flex bg-gray-800 border-gray-700 hover:scale-110 duration-700'>
-                   <div>
-                        <img className='w-full rounded-lg sm:rounded-none sm:rounded-l-lg' src='/team/nicolas.webp' alt='Nicolas' />
+                <div className='flex flex-col sm:flex-row items-center rounded-lg shadow bg-gray-800 border-gray-700 hover:scale-110 duration-700'>
+                    <div className='w-full sm:w-1/3 h-full'>
+                        <img className='max-h-max h-full aspect-auto rounded-t-lg sm:rounded-none sm:rounded-l-lg' src='/team/nicolas.webp' alt='Nicolas' />
                     </div>
-                    <div className='p-5'>
+                    <div className='w-full sm:w-2/3 p-5'>
                         <h3 className='text-xl font-bold tracking-tight text-white line-clamp-1'>
                            <div>Nicolas Morales Morales</div>
                         </h3>
@@ -45,11 +45,11 @@ const Team = () => {
                         <p className='mt-3 mb-4 font-light text-gray-400 line-clamp-3'>Programador</p>
                     </div>
                 </div> 
-                <div className='items-center rounded-lg shadow sm:flex bg-gray-800 border-gray-700 hover:scale-110 duration-700'>
-                   <div>
-                        <img className='w-full rounded-lg sm:rounded-none sm:rounded-l-lg' src='/team/emmanuel.webp' alt='Emmanuel' />
+                <div className='flex flex-col sm:flex-row items-center rounded-lg shadow bg-gray-800 border-gray-700 hover:scale-110 duration-700'>
+                    <div className='w-full sm:w-1/3 h-full'>
+                        <img className='max-h-max h-full aspect-auto rounded-t-lg sm:rounded-none sm:rounded-l-lg' src='/team/emmanuel.webp' alt='Emmanuel' />
                     </div>
-                    <div className='p-5'>
+                    <div className='w-full sm:w-2/3 p-5'>
                         <h3 className='text-xl font-bold tracking-tight text-white line-clamp-1'>
                            <div>Emmanuel Andres Soto Peña</div>
                         </h3>
