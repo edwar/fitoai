3b4cc227c8734425126f57a20cacf5c9d492cb8b
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-04-23 17:35:52 -0500

    Helps

--- src/app/Components/Card.tsx
+++ src/app/Components/Card.tsx
@@ -7,14 +7,14 @@ interface Props {
     url: string
 }
 
-export default function Card({ image, description, title, url }: Props) {
+export default function Card({ image, description, title, url }: Readonly<Props>) {
   return (
     <div className='w-full'>
         <div className='bg-slate-700 shadow-md border border-slate-500 rounded-lg'>
-            <img className='rounded-t-lg' src={image} alt={`Imagen de ${title}`} />
+            <img className='rounded-t-lg aspect-square' src={image} alt={`Imagen de ${title}`} />
             <div className='p-5'>
                 <h5 className='text-white font-bold text-2xl tracking-tight mb-2 truncate'>{title}</h5>
-                <p className='font-normal text-slate-200 mb-3 line-clamp-3'>{description}</p>
+                <p className='font-normal text-slate-200 mb-3 line-clamp-3' dangerouslySetInnerHTML={{ __html: description }}/>
                 <div className='flex justify-end'>
                     <a className='text-white bg-slate-800 hover:bg-slate-900 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center' href={url}>
                         Ver mas
---
+++ src/app/help/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { helps } from '@/app/data/help'
+
+type Props = {
+    params: {
+        slug: string
+    }
+}
+
+export default function Page({ params: { slug } } : Readonly<Props>) {
+    const help = helps.find(({ id }) => id === slug)
+  return (
+    <div className='flex gap-8 w-full flex-col items-center min-h-screen max-w-screen-2xl mx-auto pt-28'>
+        {!help ? 
+            <>
+                <p className='text-6xl font-bold'>No se encontro ningún artículo relacionado...</p>
+                <img className='aspect-auto w-1/2' src="/helps/todo-bien.webp" alt="Todo esta bien" />
+            </>:
+            <>
+                <h1 className='text-4xl font-semibold'>{help.title}</h1>
+                <img className='w-1/3 float-left' src={help.image} alt={`Imagen de ${help.title}`} />
+                <p className='text-balance font-light' dangerouslySetInnerHTML={{ __html: help.description }} />
+            </>
+        } 
+    </div>
+  )
+}
