cb0e3f6b85f484bf85d71f368b41f31eb98243c9
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-04-24 11:35:18 -0500

    Help

--- src/app/data/help.ts
+++ src/app/data/help.ts
@@ -27,33 +27,5 @@ export const helps = [
         <br/><strong class="text-2xl">5. Mosca blanca: un clon incesante</strong><br/><br/>
         
         La mosca blanca, con su aspecto similar a una polilla, se reproduce a un ritmo alarmante. Se alimenta de la savia de las plantas y deposita una sustancia pegajosa que favorece el crecimiento de hongos. Un enemigo formidable que puede causar daños considerables a los cultivos.<br/><br/>`
-    },{
-        id: "plagas-mas-comunes-2",
-        image: "/helps/plaga.webp",
-        title: "Plagas más comunes",
-        description:`<strong class="text-2xl">Enemigos invisibles:</strong> Las plagas más comunes de los cultivos frutales<br/><br/>
-        Los huertos frutales, esos oasis de vida que nos brindan deliciosas frutas, no están exentos de peligros. Entre las ramas y hojas acechan plagas, diminutos pero feroces enemigos que pueden diezmar cosechas y poner en riesgo la salud de los árboles.<br/>
-        
-        En este artículo, exploraremos algunos de los protagonistas más comunes de este drama natural: las plagas que azotan los frutales. Conocerlas es el primer paso para combatirlas y proteger nuestros preciados frutales.<br/>
-        
-        <br/><strong class="text-2xl">1. Pulgones: pequeños pero voraces</strong><br/><br/>
-        
-        Los pulgones, esos insectos diminutos de colores variados, son como vampiros para las plantas. Se alimentan de la savia, debilitando los árboles y causando daños en hojas y brotes. Además, pueden actuar como vectores de enfermedades virales.<br/>
-        
-        <br/><strong class="text-2xl">2. Mosca de la fruta: una invitada no deseada</strong><br/><br/>
-        
-        La mosca de la fruta, con su característico zumbido, pone sus huevos en el interior de las frutas maduras, generando gusanos que las pudren y las hacen incomestibles. Un verdadero festín para ellas, una pesadilla para los agricultores.<br/>
-        
-        <br/><strong class="text-2xl">3. Arañuela roja: tejedora de desgracias</strong><br/><br/>
-        
-        La arañuela roja, a pesar de su pequeño tamaño, puede causar grandes daños. Teje finas telarañas en las hojas, alimentándose de su savia y provocando que se tornen amarillas y caigan. Un ataque silencioso que debilita los árboles y reduce la producción de frutos.<br/>
-        
-        <br/><strong class="text-2xl">4. Cochinilla: un escudo que la protege</strong><br/><br/>
-        
-        La cochinilla, protegida por un caparazón ceroso, se adhiere a las ramas y hojas, succionando su savia. Debilita las plantas y las hace propensas a otras plagas y enfermedades. Además, segrega una sustancia azucarada que atrae a las hormigas, creando un ecosistema indeseable en el árbol.<br/>
-        
-        <br/><strong class="text-2xl">5. Mosca blanca: un clon incesante</strong><br/><br/>
-        
-        La mosca blanca, con su aspecto similar a una polilla, se reproduce a un ritmo alarmante. Se alimenta de la savia de las plantas y deposita una sustancia pegajosa que favorece el crecimiento de hongos. Un enemigo formidable que puede causar daños considerables a los cultivos.<br/><br/>`
     }
 ]
