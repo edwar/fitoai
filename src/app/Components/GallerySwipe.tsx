eae2cd563c60d013b52d8544eb4c9e39cc3a4a68
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-05-06 19:29:09 -0500

    Nav gallery

--- src/app/Components/GallerySwipe.tsx
+++ src/app/Components/GallerySwipe.tsx
@@ -38,11 +38,11 @@ export default function GallerySwipe({ sources, galleryID }: Readonly<{sources: 
   return (
     <div className="grid grid-cols-3 gap-4">
       {sources.map((source, index) => (
-        <div key={`${galleryID} - ${index}`} className="w-full cursor-pointer" onClick={() => { openLightbox(index) }}>
+        <div key={`${galleryID} - ${index}`} className="flex justify-center items-center w-full cursor-pointer" onClick={() => { openLightbox(index) }}>
           {source.resource_type === 'image' ? (
-            <img src={source.thumbnailURL} alt={source.alt} className="w-full h-auto pointer-events-none" />
+            <img src={source.thumbnailURL} alt={source.alt} className="w-full aspect-square pointer-events-none" />
           ) : (
-            <video controls className="w-full h-full aspect-video pointer-events-none">
+            <video controls className="w-full aspect-square pointer-events-none">
               <source src={source.thumbnailURL} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
@@ -61,7 +61,7 @@ export default function GallerySwipe({ sources, galleryID }: Readonly<{sources: 
               onMoveNextRequest={moveNext}
             />
           ) : (
-            <div className="absolute z-40 top-0 left-0 bottom-0 bg-black/90">
+            <div className="fixed inset-0 z-50 bottom-0 bg-black/90">
               <video controls autoPlay className="w-screen h-screen aspect-square">
                 <source src={sources[currentItem].largeURL} type="video/mp4" />
                 Your browser does not support the video tag.
@@ -73,6 +73,18 @@ export default function GallerySwipe({ sources, galleryID }: Readonly<{sources: 
                   <path d="M6 6l12 12" />
                 </svg>
               </button>
+                <button className='absolute z-50 top-1/2 -left-1 transform -translate-y-1/2' onClick={movePrev}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-400 w-16 h-14 hover:text-white" width="68" height="68" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M15 6l-6 6l6 6" />
+                  </svg>
+                </button>
+                <button className='absolute z-50 top-1/2 right-0 transform -translate-y-1/2' onClick={moveNext}>
+                  <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-400 w-14 h-14 hover:text-white" width="68" height="68" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M9 6l6 6l-6 6" />
+                  </svg>
+                </button>
             </div>
           )}
         </div>
