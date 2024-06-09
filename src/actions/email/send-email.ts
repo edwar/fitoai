c0f48fea2b89e337e183f65c6ba828459c2c2860
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-06-08 19:38:17 -0500

    Send email serve action

--- src/actions/email/send-email.ts
+++ src/actions/email/send-email.ts
@@ -1,33 +1,63 @@
-'use server';
-import nodemailer from 'nodemailer';
+"use server";
+import nodemailer from "nodemailer";
 
 interface MailOptions {
-  from: string;
+  from: {
+    name: string;
+    address: string;
+  };
   to: string;
   subject: string;
   text: string;
 }
 
-export const sendEmail = (formData: FormData) => {
+export const sendEmail = async (formData: FormData) => {
   const mailOptions: MailOptions = {
-    from: `${formData.get('email')}`,
-    subject: `${formData.get('subject')} - Mensaje de ${formData.get('names')} ${formData.get('lastNames')} - ${formData.get('email')}`,
-    text: `${formData.get('text')}`,
-    to: 'mrmoralestech@gmail.com'
-  }
-  console.log({mailOptions})
+    from: {
+      name: `${formData.get("names")} ${formData.get("lastNames")}`,
+      address: "myEmail@gmail.com",
+    },
+    subject: `${formData.get("subject")} - Mensaje de ${formData.get(
+      "names"
+    )} ${formData.get("lastNames")} - ${formData.get("email")}`,
+    text: `${formData.get("text")}`,
+    to: "mrmoralestech@gmail.com",
+  };
+  console.log({ mailOptions });
   let transporter = nodemailer.createTransport({
-    service: 'Gmail', // Use your email provider
+    port: 465,
+    host: "smtp.gmail.com", // Use your email provider
     auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD
-    }
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+    secure: true,
   });
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
         console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
   });
-}
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
+  });
+  return {
+    ok: true,
+  };
+};
