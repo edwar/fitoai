f26a3dd03169b6457e1d097ed6c2480343cdae1f
Edwar Amaya <edwar.amaya@ppm.com.ec>
2024-08-25 17:54:31 -0500

    loading

--- src/app/Components/Loading.tsx
+++ src/app/Components/Loading.tsx
@@ -2,10 +2,27 @@ import React from 'react'
 
 const Loading = () => {
   return <div className='absolute inset-0 overflow-hidden bg-slate-900 flex items-center justify-center z-50'>
-    <div className='relative'>
-        <div className='h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200'></div>
-        <div className='absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-teal-600 animate-spin'>
-        </div>
+    <div className="night">
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
+      <div className="shooting_star"></div>
     </div>
 </div>
 }
